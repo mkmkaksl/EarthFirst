3788b26f571708d1dce0bb86aafaf454da643a6a
mkmkaksl <manojk@4146c987a9a6>
2024-01-21 00:42:04 +0000

    Connected To PythonAnywhere

--- EarthFirst/settings.py
+++ EarthFirst/settings.py
@@ -24,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i=way&j-q(_^2)es@vx^40ktv)$%7diqmvw08!(o$+ig6kl^eh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'manojk.pythonanywhere.com',
-    "127.0.0.1"
 ]
 
 
@@ -36,7 +35,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'greenClean',
-    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Media files
 MEDIA_URL = '/media/'
--- greenClean/views.py
+++ greenClean/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.staticfiles import finders
-from django.conf import settings
+# from django.http import HttpResponseRedirect
+# from django.urls import reverse
+# from django.contrib.staticfiles import finders
+# from django.conf import settings
+from django.core.files.storage import default_storage
 from django import forms
 
 # for map visualization
-import numpy as np
+# import numpy as np
 import pandas as pd
-import plotly as py
-import os
+# import plotly as py
+# import os
 import requests
 from io import StringIO
-import plotly.express as px
+# import plotly.express as px
 import plotly.graph_objs as go
-from plotly.subplots import make_subplots
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# from plotly.subplots import make_subplots
+# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-worldData = requests.get("https://energy-data.netlify.app/").text
-world_csv = pd.read_csv(StringIO(worldData))
+# worldData = requests.get("https://energy-data.netlify.app/").text
+# worldData = "country,year,iso_code,population,gdp,biofuel_cons_change_pct,biofuel_cons_change_twh,biofuel_cons_per_capita,biofuel_consumption,biofuel_elec_per_capita,biofuel_electricity,biofuel_share_elec,biofuel_share_energy,carbon_intensity_elec,coal_cons_change_pct,coal_cons_change_twh,coal_cons_per_capita,coal_consumption,coal_elec_per_capita,coal_electricity,coal_prod_change_pct,coal_prod_change_twh,coal_prod_per_capita,coal_production,coal_share_elec,coal_share_energy,electricity_demand,electricity_generation,electricity_share_energy,energy_cons_change_pct,energy_cons_change_twh,energy_per_capita,energy_per_gdp,fossil_cons_change_pct,fossil_cons_change_twh,fossil_elec_per_capita,fossil_electricity,fossil_energy_per_capita,fossil_fuel_consumption,fossil_share_elec,fossil_share_energy,gas_cons_change_pct,gas_cons_change_twh,gas_consumption,gas_elec_per_capita,gas_electricity,gas_energy_per_capita,gas_prod_change_pct,gas_prod_change_twh,gas_prod_per_capita,gas_production,gas_share_elec,gas_share_energy,greenhouse_gas_emissions,hydro_cons_change_pct,hydro_cons_change_twh,hydro_consumption,hydro_elec_per_capita,hydro_electricity,hydro_energy_per_capita,hydro_share_elec,hydro_share_energy,low_carbon_cons_change_pct,low_carbon_cons_change_twh,low_carbon_consumption,low_carbon_elec_per_capita,low_carbon_electricity,low_carbon_energy_per_capita,low_carbon_share_elec,low_carbon_share_energy,net_elec_imports,net_elec_imports_share_demand,nuclear_cons_change_pct,nuclear_cons_change_twh,nuclear_consumption,nuclear_elec_per_capita,nuclear_electricity,nuclear_energy_per_capita,nuclear_share_elec,nuclear_share_energy,oil_cons_change_pct,oil_cons_change_twh,oil_consumption,oil_elec_per_capita,oil_electricity,oil_energy_per_capita,oil_prod_change_pct,oil_prod_change_twh,oil_prod_per_capita,oil_production,oil_share_elec,oil_share_energy,other_renewable_consumption,other_renewable_electricity,other_renewable_exc_biofuel_electricity,other_renewables_cons_change_pct,other_renewables_cons_change_twh,other_renewables_elec_per_capita,other_renewables_elec_per_capita_exc_biofuel,other_renewables_energy_per_capita,other_renewables_share_elec,other_renewables_share_elec_exc_biofuel,other_renewables_share_energy,per_capita_electricity,primary_energy_consumption,renewables_cons_change_pct,renewables_cons_change_twh,renewables_consumption,renewables_elec_per_capita,renewables_electricity,renewables_energy_per_capita,renewables_share_elec,renewables_share_energy,solar_cons_change_pct,solar_cons_change_twh,solar_consumption,solar_elec_per_capita,solar_electricity,solar_energy_per_capita,solar_share_elec,solar_share_energy,wind_cons_change_pct,wind_cons_change_twh,wind_consumption,wind_elec_per_capita,wind_electricity,wind_energy_per_capita,wind_share_elec,wind_share_energy Afghanistan,1986,AFG,10448447,17641345024.000,,,,,,,,,,,,,,,,5.960,0.052,88.904,0.929,,,,,,2.185,0.245,1097.040,0.650,,,,,,,,,,,,,,,0.000,0.000,3086.329,32.247,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,0.000,0.000,0.000,,,,,,,,,,,,,,,11.462,,,,,,,,,,,,,,,,,,,,,,,, Afghanistan,1987,AFG,10322767,15810816000.000,,,,,,,,,,,,,,,,4.375,0.041,93.924,0.970,,,,,,61.246,7.020,1790.467,1.169,,,,,,,,,,,,,,,-5.714,-1.843,2945.396,30.405,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,0.000,0.000,0.000,,,,,,,,,,,,,,,18.483,,,,,,,,,,,,,,,,,,,,,,,, Afghanistan,1988,AFG,10383459,14499068928.000,,,,,,,,,,,,,,,,-17.365,-0.168,77.160,0.801,,,,,,76.359,14.113,3139.185,2.248,,,,,,,,,,,,,,,7.071,2.150,3135.223,32.554,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,0.000,0.000,0.000,,,,,,,,,,,,,,,32.596,,,,,,,,,,,,,,,,,,,,,,,,"
+# worldData = open("owid-energy-data.csv")
+worldData = default_storage.path("energyData.csv")
+# world_csv = pd.read_csv(StringIO(worldData))
+world_csv = pd.read_csv(worldData)
+
 #data cleaning
 world_csv = world_csv.drop('gdp', axis=1) #remove gdp column
 world_csv = world_csv.drop('population', axis=1) #remove population column
 
 country_sorted = world_csv.groupby(['country', 'year']).sum().reset_index().sort_values('year', ascending=False)
 
-maxYear = max(country_sorted["year"])
-minYear = min(country_sorted["year"])
+# maxYear = max(country_sorted["year"])
+# minYear = min(country_sorted["year"])
+maxYear = 2022
+minYear = 1986
 
 # Mapping choice to it's visual text for select field
 categories = [
@@ -95,7 +103,7 @@ def map(request):
 
     fig = go.Figure(
         data=go.Choropleth(
-            locations=year_countries["country"], 
+            locations=year_countries["country"],
             locationmode="country names",
             z=year_countries[col],
             colorscale = 'Greens',
